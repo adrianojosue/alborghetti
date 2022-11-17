c6e802a501de28f7d66903c811a1e0b2706a9b0e
adrianojosue <josuediazhidalgo@gmail.com>
2022-11-17 09:49:29 -0400

    Added some styles at the help section

--- src/components/help.js
+++ src/components/help.js
@@ -7,10 +7,19 @@ export const HelpScreen = () => {
             <motion.div className="content_normal help" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
                 <div className="customer-image margin-bottom_medium"></div>
 
-                <h1 className="margin-bottom_medium">How can we help you<motion.span initial={{ rotate: 0, scale: 1 }} animate={{ rotate: 24, scale: 1.5 }} transition={{ duration: 0.8, repeat: Infinity, repeatType: 'reverse' }}>?</motion.span></h1>
+                <motion.h1
+                    className="margin-bottom_medium align-center"
+                    initial={{opacity: 0, scale: 0.5 }}
+                    animate={{opacity: 1, scale: 1 }}
+                    exit={{opacity: 0, scale: 0.5 }}
+                >How can we help you
+                <motion.span initial={{ rotate: 0, scale: 1 }} animate={{ rotate: 24, scale: 1.5 }} transition={{ duration: 0.8, repeat: Infinity, repeatType: 'reverse' }}>
+                    ?
+                </motion.span>
+                </motion.h1>
 
                 <div>
-                    <h2 className="margin-bottom_x-small">Contact aur customer service</h2>
+                    <h2 className="margin-bottom_x-small padding-bottom_x-small border-bottom">Contact aur customer service</h2>
 
                     <h3>Tel:</h3>
                     <span className="margin-bottom_small">
@@ -19,15 +28,17 @@ export const HelpScreen = () => {
 
                     <h3>Email:</h3>
                     <span className="margin-bottom_small">
-                        <a href="mailto:help@alborghetti.store">help@alborghetti.store</a>
+                        <a href="mailto:help@email.com">help@email.com</a>
                     </span>
                     
-                    <h2 className="margin-bottom_x-small">Schedule</h2>
+                    <hr/>
 
-                    <h3>MON-FRI:</h3>
+                    <h2 className="margin-bottom_x-small padding-bottom_x-small border-bottom">Schedule</h2>
+
+                    <h3>Mon - Fri:</h3>
                     <span className="margin-bottom_small">8:30 AM to 7:30 PM (AST)</span>
 
-                    <h3>SAT:</h3>
+                    <h3>Sat:</h3>
                     <span>10:00 AM to 4:00 PM (AST)</span>
                 </div>
             </motion.div>
